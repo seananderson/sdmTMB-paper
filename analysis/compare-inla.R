e1427b6e657d4c5147c2a1cc2514b13edfd0913d
Sean Anderson <sean@seananderson.ca>
2021-07-16 18:50:37 -0700

    Add terrible copy-pasted comparison to INLA

--- analysis/compare-inla.R
+++ analysis/compare-inla.R
@@ -9,8 +9,8 @@ mesh <- make_mesh(d, xy_cols = c("X", "Y"), cutoff = 5)
 plot(mesh)
 tictoc::tic("INLA")
 spde <- inla.spde2.pcmatern(mesh = mesh$mesh,
-  prior.range = c(5, 0.05), # P(range < 0.05) = 0.01
-  prior.sigma = c(5, 0.05)) # P(sigma > 1) = 0.01
+  prior.range = c(5, 0.05), # P(range < 5) = 0.05
+  prior.sigma = c(5, 0.05)) # P(sigma > 5) = 0.05
 iset <- inla.spde.make.index('i', n.spde = spde$n.spde,
   n.group = nyear)
 A <- inla.spde.make.A(mesh = mesh$mesh,
@@ -32,12 +32,11 @@ m_inla <- inla(formulae,
   control.predictor = list(compute = TRUE, A = inla.stack.A(sdat)),
   family = "binomial",
   # control.family = list(hyper = list(theta = prec.prior)),
-  control.fixed = list(expand.factor.strategy = 'inla', mean = 0, prec = 0.0001),
+  control.fixed = list(expand.factor.strategy = 'inla', mean = 0, prec = 1/(100*100)),
   verbose = FALSE,
   control.compute = list(config = TRUE)
   )
-# post_inla <- inla.posterior.sample(result = m_inla, n = 30L)
-# a <- unlist(lapply(post_inla, function(s) s$latent["(Intercept)", 1]))
+post_inla <- inla.posterior.sample(result = m_inla, n = 400L)
 # head(post_inla)
 tictoc::toc()
 summary(m_inla)
@@ -48,22 +47,32 @@ summary(m_inla)
 # inla.set.control.fixed.default()[c("mean.intercept", "prec.intercept", "mean", "prec")]
 # https://www.paulamoraga.com/book-geospatial/sec-inla.html
 
-
 tictoc::tic("sdmTMB")
 m <- sdmTMB(present ~ 0 + as.factor(year), data = d, spde = mesh, time = "year",
   fields = "AR1", include_spatial = FALSE, family = binomial(link = "logit"),
   reml = FALSE,
   priors = sdmTMBpriors(
     matern_st = pc_matern(range_gt = 5, range_prob = 0.05, sigma_lt = 5, sigma_prob = 0.05),
-    ar1_rho = normal(0, 1)
+    ar1_rho = normal(0, 1),
+    b = normal(0, 100)
   ))
-post_sdmTMB <- gather_sims(m, n_sims = 400L)
+post_sdmTMB <- gather_sims(m, n_sims = 2000L)
 tictoc::toc()
 m
 
 head(post_sdmTMB)
 
+library(dplyr)
+library(ggplot2)
+year_factor2011_inla <- unlist(lapply(post_inla, function(s) s$latent["year_factor2017:1", 1]))
+year_factor2011_tmb <- filter(post_sdmTMB, .variable == "as.factor.year.2017") %>% pull(.value)
+
+ggplot(bind_rows(data.frame(type = "INLA", post = year_factor2011_inla),
+  data.frame(type = "sdmTMB", post = year_factor2011_tmb)), aes(post, colour = type)) +
+  geom_density()
+
 plot(inla.smarginal(m_inla$marginals.fixed$year_factor2011), type = "l")
+hist(post_sdmTMB)
 
 # inla.smarginal(m_inla$marginals.hyperpar$`Range for i`)
 
@@ -90,5 +99,74 @@ inla.qmarginal(0.975, m_inla$marginals.hyperpar$`GroupRho for i`)
 tidy(m, conf.int = TRUE)
 tidy(m, "ran_pars", conf.int = TRUE)
 
+x <- inla.smarginal(m_inla$marginals.fixed$year_factor2013)
+b <- filter(post_sdmTMB, .variable == "as.factor.year.2013") %>% pull(.value)
+g2 <- ggplot(as.data.frame(x), aes(x, y)) + geom_line() +
+  geom_histogram(data = data.frame(x = b), mapping = aes(x, after_stat(density)),
+    inherit.aes = FALSE, binwidth = 0.1, fill = "blue", alpha = 0.2) +
+  ggtitle("Year 2013")
+
+x <- inla.smarginal(m_inla$marginals.fixed$year_factor2011)
+b <- filter(post_sdmTMB, .variable == "as.factor.year.2011") %>% pull(.value)
+g1 <- ggplot(as.data.frame(x), aes(x, y)) + geom_line() +
+  geom_histogram(data = data.frame(x = b), mapping = aes(x, after_stat(density)),
+    inherit.aes = FALSE, binwidth = 0.1, fill = "blue", alpha = 0.2) +
+  ggtitle("Year 2011")
+
+x <- inla.smarginal(m_inla$marginals.fixed$year_factor2015)
+b <- filter(post_sdmTMB, .variable == "as.factor.year.2015") %>% pull(.value)
+g2015 <- ggplot(as.data.frame(x), aes(x, y)) + geom_line() +
+  geom_histogram(data = data.frame(x = b), mapping = aes(x, after_stat(density)),
+    inherit.aes = FALSE, binwidth = 0.1, fill = "blue", alpha = 0.2) +
+  ggtitle("Year 2015")
+
+x <- inla.smarginal(m_inla$marginals.fixed$year_factor2017)
+b <- filter(post_sdmTMB, .variable == "as.factor.year.2017") %>% pull(.value)
+g2017 <- ggplot(as.data.frame(x), aes(x, y)) + geom_line() +
+  geom_histogram(data = data.frame(x = b), mapping = aes(x, after_stat(density)),
+    inherit.aes = FALSE, binwidth = 0.1, fill = "blue", alpha = 0.2) +
+  ggtitle("Year 2017")
+
+x <- inla.smarginal(m_inla$marginals.hyperpar$`Range for i`)
+range_post <- filter(post_sdmTMB, .variable == "range") %>% pull(.value)
+g3 <- ggplot(as.data.frame(x), aes(x, y)) + geom_line() +
+  geom_histogram(data = data.frame(x = range_post), mapping = aes(x, after_stat(density)),
+    inherit.aes = FALSE, binwidth = 1.5, fill = "blue", alpha = 0.2) +
+  ggtitle("Range")
+
+x <- inla.smarginal(m_inla$marginals.hyperpar$`Stdev for i`)
+sigma_E_post <- filter(post_sdmTMB, .variable == "sigma_E") %>% pull(.value)
+g4 <- ggplot(as.data.frame(x), aes(x, y)) + geom_line() +
+  geom_histogram(data = data.frame(x = sigma_E_post), mapping = aes(x, after_stat(density)),
+    inherit.aes = FALSE, binwidth = 0.1, fill = "blue", alpha = 0.2) +
+  ggtitle("Random Field SD")
+
+x <- inla.smarginal(m_inla$marginals.hyperpar$`GroupRho for i`)
+rho_post <- filter(post_sdmTMB, .variable == "ar1_rho") %>% pull(.value)
+g5 <- ggplot(as.data.frame(x), aes(x, y)) + geom_line() +
+  geom_histogram(data = data.frame(x = rho_post), mapping = aes(x, after_stat(density)),
+    inherit.aes = FALSE, binwidth = 0.02, fill = "blue", alpha = 0.2) +
+  ggtitle("AR1 rho")
+
+cowplot::plot_grid(g1, g2, g2015, g2017, g3, g4, g5)
+
 # ------------------
 
+
+# library(inlabru)
+# library(tidyverse)
+# data <- data.frame(name_idx = 1:100) %>%
+#   mutate(name_val = rnorm(100),
+#     mu = 100 * exp(name_val),
+#     y = rpois(100, mu))
+# comp <- ~ Intercept(1) + name(name_idx, model = "iid", mapper = bru_mapper_index(100))
+# fit <- bru(comp, formula = y ~ Intercept + name, family = "poisson", data = data)
+# newdata <- data.frame(name_idx = 1:200)
+# gen <- generate(fit,
+#   data = newdata,
+#   formula = ~ exp(Intercept + name_eval(name_idx)),
+#   n.samples = 1)
+# ggplot(data, aes(name_idx, mu)) +
+#   geom_point(aes(col = "Observed")) +
+#   geom_point(aes(col = "Generated"), data = cbind(newdata, mu = gen[,1])) +
+#   scale_y_log10()
