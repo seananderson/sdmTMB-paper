6b0ccb06c6a0b9a5391a6da1a7ed334c30eeac67
Sean Anderson <sean@seananderson.ca>
2021-11-19 13:11:29 -0800

    Commit timing before moving to INLA

--- analysis/timing.R
+++ analysis/timing.R
@@ -5,7 +5,9 @@ library(INLA)
 library(inlabru)
 # library(tictoc)
 library(future)
-plan(multisession, workers = 8L)
+plan(multisession)
+
+# JW: you should see linear growth in n_obs and O(n_mesh^(3/2)) growth in n_mesh
 
 simulate_dat <- function(n_obs = 100,
                          cutoff = 0.1,
@@ -22,7 +24,6 @@ simulate_dat <- function(n_obs = 100,
     a1 = rnorm(n_obs)
   )
   mesh <- make_mesh(predictor_dat, xy_cols = c("X", "Y"), cutoff = cutoff)
-  mesh$mesh$n
   sim_dat <- sdmTMB_simulate(
     formula = ~ 1 + a1,
     data = predictor_dat,
@@ -42,12 +43,13 @@ simulate_dat <- function(n_obs = 100,
   }
   list(mesh = mesh, dat = sim_dat)
 }
-sim_fit_time <- function(n_obs = 1000, cutoff = 0.1, iter = 1, seed = sample.int(.Machine$integer.max, 1)) {
+
+sim_fit_time <- function(n_obs = 1000, cutoff = 0.1, iter = 1, seed = sample.int(.Machine$integer.max, 1), family = gaussian()) {
   cat("cutoff:", cutoff, "\n")
   cat("n_obs:", n_obs, "\n")
   cat("iter:", iter, "\n")
   cat("simulating...\n")
-  s <- simulate_dat(n_obs = n_obs, cutoff = cutoff, seed = seed, iter = iter)
+  s <- simulate_dat(n_obs = n_obs, cutoff = cutoff, seed = seed, iter = iter, family = family)
   sim_dat <- s$dat
   mesh <- s$mesh
   times <- list()
@@ -56,7 +58,7 @@ sim_fit_time <- function(n_obs = 1000, cutoff = 0.1, iter = 1, seed = sample.int
   cat("sdmTMB\n")
   out <- system.time({
     fit <- sdmTMB(observed ~ a1,
-      data = sim_dat, mesh = mesh,
+      data = sim_dat, mesh = mesh, family = family,
       priors = sdmTMBpriors(matern_s = pc_matern(range_gt = 0.05, sigma_lt = 2))
     )
   })
@@ -65,46 +67,47 @@ sim_fit_time <- function(n_obs = 1000, cutoff = 0.1, iter = 1, seed = sample.int
   cat("sdmTMB normalize\n")
   out <- system.time({
     fit2 <- sdmTMB(observed ~ a1,
-      data = sim_dat, mesh = mesh,
+      data = sim_dat, mesh = mesh, family = family,
       priors = sdmTMBpriors(matern_s = pc_matern(range_gt = 0.05, sigma_lt = 2)),
       control = sdmTMBcontrol(normalize = TRUE)
     )
   })
   times$sdmTMB_norm <- out[["elapsed"]]
 
-  spde <- inla.spde2.pcmatern(
-    mesh = mesh$mesh,
-    prior.range = c(0.05, 0.05), # P(range < 10) = 0.05
+  # loc_xy <- as.matrix(sim_dat[,c("X", "Y")])
+  sp::coordinates(sim_dat) <- c("X", "Y")
+  mesh_inla <- INLA::inla.mesh.create(sim_dat, refine = TRUE, cutoff = cutoff)
+  spde <- INLA::inla.spde2.pcmatern(
+    mesh = mesh_inla,
+    prior.range = c(0.05, 0.05),
     prior.sigma = c(2, 0.05)
-  ) # P(sigma > 2) = 0.05
-  components <- observed ~ a1 + spatrf(main = coordinates, model = spde)
+  )
+  components <- observed ~ -1 + Intercept(1) + a1 + spatrf(main = coordinates, model = spde)
+  like <- like(observed ~ Intercept + a1 + spatrf, family = "gaussian", data = sim_dat)
   cat("inlabru\n")
   out <- system.time({
     fit_bru <- bru(
-      components = components,
-      family = "gaussian", data = sim_dat, options = bru_options(bru_verbose = FALSE)
+      like,
+      components = components
     )
   })
   times$inlabru <- out[["elapsed"]]
 
-  # bru_coefs <- fit_bru$summary.fixed[, c("mean", "sd")]
-  # bru_coefs
+  # fit_bru$summary.fixed[, c("mean", "sd")]
   # fit_bru$summary.hyperpar[,c("mean", "sd")]
   # 1 / sqrt(fit_bru$summary.hyperpar[1,c("mean")]) # N(0, sigma^2)
-  #
 
   cat("inlabru eb\n\n")
   out <- system.time({
     fit_bru2 <- bru(
+      like,
       components = components,
-      family = "gaussian", data = sim_dat,
-      options = bru_options(bru_verbose = FALSE, control.inla = list(int.strategy = "eb"))
+      options = bru_options(control.inla = list(int.strategy = "eb"))
     )
   })
   times$inlabru_eb <- out[["elapsed"]]
 
-  # bru_coefs <- fit_bru2$summary.fixed[, c("mean", "sd")]
-  # bru_coefs
+  # fit_bru2$summary.fixed[, c("mean", "sd")]
   # fit_bru2$summary.hyperpar[,c("mean", "sd")]
   # 1 / sqrt(fit_bru2$summary.hyperpar[1,c("mean")]) # N(0, sigma^2)
 
@@ -117,15 +120,20 @@ sim_fit_time <- function(n_obs = 1000, cutoff = 0.1, iter = 1, seed = sample.int
   out
 }
 
-test <- sim_fit_time(n_obs = 100, cutoff = 0.5)
+test <- sim_fit_time(n_obs = 300, cutoff = 0.05, family = gaussian())
 test
 
 to_run <- expand.grid(
-  n_obs = c(100, 200, 500, 1000, 2000, 5000),
-  cutoff = c(0.2, 0.1, 0.05, 0.025),
-  iter = seq_len(2L)
+  # n_obs = c(100, 200, 500, 1000, 2000, 5000),
+  # n_obs = c(100, 500, 2000),
+  n_obs = c(500),
+  cutoff = c(0.1, 0.05, 0.04, 0.02),
+  iter = seq_len(1L)
 )
 to_run$seed <- to_run$iter * 29172830
+
+# scramble for parallel task assignment:
+to_run <- to_run[sample(seq_len(nrow(to_run)), replace = FALSE), ]
 
 # out <- purrr::pmap_dfr(to_run, sim_fit_time)
 out <- furrr::future_pmap_dfr(
@@ -136,6 +144,7 @@ out <- furrr::future_pmap_dfr(
 )
 saveRDS(out, file = "analysis/timing-cache.rds")
 out <- readRDS("analysis/timing-cache.rds")
+plan(sequential)
 
 out_long <- tidyr::pivot_longer(
   out,
@@ -158,6 +167,8 @@ out_long <- out_long %>%
 out_long$mean_mesh_n <- as.factor(out_long$mean_mesh_n)
 out_long$mean_mesh_n <- forcats::fct_reorder(out_long$mean_mesh_n, -out_long$cutoff)
 out_long <- filter(out_long, n_obs <= 5000)
+out_long <- group_by(out_long, model_clean, n_obs, mean_mesh_n) %>%
+  summarise(time = mean(time))
 g <- ggplot(out_long, aes(n_obs, time, colour = model_clean)) +
   geom_line(lwd = 0.7) +
   scale_y_log10() +
