3bcfe62e851bafcaf1688611bda78aeb46ad9cab
Sean Anderson <sean@seananderson.ca>
2022-01-14 13:10:29 -0800

    Add make.R

---
+++ doc/make.R
@@ -0,0 +1,36 @@
+files <- c(
+  "paper",
+  "appendix-model",
+  "appendix-pcod-sdmTMB",
+  "appendix-binomial-inla",
+  "appendix-pcod-VAST-tweedie",
+  "appendix-validation"
+)
+
+# https://github.com/rstudio/rmarkdown/issues/1673
+render_separately <- function(...) callr::r(
+  function(...) rmarkdown::render(..., envir = globalenv()), args = list(...), show = TRUE)
+
+for (i in seq_along(files)) {
+  render_separately(paste0(here::here("doc", files[i]), ".Rmd"))
+}
+
+names_to <- c(
+  "sdmTMB-paper.pdf",
+  "appendix1-model.pdf",
+  "appendix2-index-sdmTMB.pdf",
+  "appendix3-INLA-comparison.pdf",
+  "appendix4-VAST-comparison.pdf",
+  "appendix5-speed-validation.pdf"
+)
+
+loc <- "~/Dropbox/sdmTMB-paper/"
+
+for (i in seq_along(files)) {
+  from <- paste0(here::here("doc", files[i]), ".pdf")
+  to <- paste0(loc, names_to[i])
+  cat(to, "\n")
+  file.copy(from, to, overwrite = TRUE)
+}
+
+system("cd doc; ./clean-bib.sh")
