2dd8587da702876cf623e64a38f1812098e4a68a
Sean Anderson <sean@seananderson.ca>
2024-02-09 16:17:35 -0800

    Update make-reprex-jss.R

--- doc/paper-jss/make-reprex-jss.R
+++ doc/paper-jss/make-reprex-jss.R
@@ -4,14 +4,13 @@ setwd(here::here())
 setwd("doc/paper-jss/")
 dir.create("reprex", showWarnings = FALSE)
 
-knitr::purl("paper-jss.Rmd", documentation = 1L)
+knitr::purl("sdmTMB-paper.Rnw", documentation = 1L)
 
-system("mv paper-jss.R reprex/paper-jss.R")
+system("mv sdmTMB-paper.R reprex/sdmTMB-paper.R")
 
-d <- readLines("reprex/paper-jss.R")
-d[grepl("## ----child=", d)]
-d <- d[!grepl("## ----child=", d)]
-
+d <- readLines("reprex/sdmTMB-paper.R")
+# d[grepl("## ----child=", d)]
+# d <- d[!grepl("## ----child=", d)]
 
 do_replace <- function(find, replacement) {
   cat("found:\n")
@@ -23,8 +22,11 @@ do_replace <- function(find, replacement) {
   d
 }
 
-d[grep("dpi = 140", d)]
-d <- do_replace("dpi = 140", "  dpi = 140")
+d[grep("knitr::render_sweave()", d)]
+d <- do_replace("knitr::render_sweave()", "# knitr::render_sweave()")
+
+d[grep("dpi = 140,", d)]
+d <- do_replace("dpi = 140,", "  dpi = 140,")
 
 d[grep(" cache = TRUE,", d)]
 d <- do_replace(" cache = TRUE,", " # cache = TRUE,")
@@ -36,23 +38,23 @@ d <- do_replace("-strip all", "  # optipng = \"-strip all\"")
 d[grep("optipng = knitr", d)]
 d <- do_replace("optipng = knitr", "# knitr::knit_hooks$set(optipng = knitr::hook_optipng)")
 
-i <- grep("## ----compare-table", d)
-j <- grep("## ----setup-pcod", d)
-d <- d[-seq(i, j-2)]
+# i <- grep("## ----compare-table", d)
+# j <- grep("## ----setup-pcod", d)
+# d <- d[-seq(i, j-2)]
 
-i <- grep("owl-knitr-setup", d)
-j <- grep("setup-owls", d)
-d <- d[-seq(i, j-1)]
+# i <- grep("owl-knitr-setup", d)
+# j <- grep("setup-owls", d)
+# d <- d[-seq(i, j-1)]
 
 
 d[grep("snow <- read", d)]
 d <- do_replace("snow <- read", "snow <- readRDS(\"snow-data.rds\")")
 
-d[grep("owl <- here::", d)]
-d <- do_replace("owl <- here::", "# owl <- here::here(\"figs\", \"owl-nao-effect.png\")")
+# d[grep("owl <- here::", d)]
+# d <- do_replace("owl <- here::", "# owl <- here::here(\"figs\", \"owl-nao-effect.png\")")
 
-d[grep("knitr::include_graphics\\(owl", d)]
-d <- do_replace("knitr::include_graphics\\(owl", "# knitr::include_graphics(owl)")
+# d[grep("knitr::include_graphics\\(owl", d)]
+# d <- do_replace("knitr::include_graphics\\(owl", "# knitr::include_graphics(owl)")
 
 d[grep("data/ne_10m_lakes", d)]
 d <- gsub("data/ne_10m_lakes", "ne_10m_lakes", d)
@@ -71,14 +73,14 @@ d <- c(d, "sessionInfo()")
 
 d[grepl("here::", d)]
 
-i <- grep("## ----setup-owls", d)
-d <- c(d[seq(1, i-1)], "\n# Snowy Owl appendix -----------------------------------------------------------------------\n", d[seq(i, length(d))])
+# i <- grep("## ----setup-owls", d)
+# d <- c(d[seq(1, i-1)], "\n# Snowy Owl appendix -----------------------------------------------------------------------\n", d[seq(i, length(d))])
 
-i <- grep("## ----setup-pcod", d)
-d <- c(d[seq(1, i-1)], "\n# Pacific Cod appendix -----------------------------------------------------------------------\n", d[seq(i, length(d))])
+# i <- grep("## ----setup-pcod", d)
+# d <- c(d[seq(1, i-1)], "\n# Pacific Cod appendix -----------------------------------------------------------------------\n", d[seq(i, length(d))])
 
-i <- grep("## ----inla-knitr-setup", d)
-d <- c(d[seq(1, i-1)], "\n# INLA comparison appendix -----------------------------------------------------------------------\n", d[seq(i, length(d))])
+# i <- grep("## ----inla-knitr-setup", d)
+# d <- c(d[seq(1, i-1)], "\n# INLA comparison appendix -----------------------------------------------------------------------\n", d[seq(i, length(d))])
 
 # these make figs not show up in html:
 d <- gsub('dev = \\"pdf\\"', 'dev = \\"png\\"', d)
@@ -91,18 +93,18 @@ writeLines(d, "reprex/replication-code-main.R")
 
 system("cp ~/src/sdmTMB-paper/data/SNOW_data.rds ~/src/sdmTMB-paper/doc/paper-jss/reprex/snow-data.rds")
 
-system("rm reprex/paper-jss.R")
+system("rm reprex/sdmTMB-paper.R")
 
 setwd("reprex")
 if (RUN_SPIN) system("R -e 'knitr::spin(\"replication-code-main.R\")'")
 
 d <- readLines(here::here("analysis/timing.R"))
 writeLines(d, "replication-code-timing.R")
-if (RUN_SPIN) system("R -e 'knitr::spin(\"replication-code-timing.R\")'")
-
-d <- readLines(here::here("analysis/pcod-fig.R"))
-# d <- gsub('silent = TRUE', 'silent = FALSE', d)
-writeLines(d, "replication-code-pcod.R")
-if (RUN_SPIN) system("R -e 'knitr::spin(\"replication-code-pcod.R\")'")
+# if (RUN_SPIN) system("R -e 'knitr::spin(\"replication-code-timing.R\")'")
+#
+# d <- readLines(here::here("analysis/pcod-fig.R"))
+# # d <- gsub('silent = TRUE', 'silent = FALSE', d)
+# writeLines(d, "replication-code-pcod.R")
+# if (RUN_SPIN) system("R -e 'knitr::spin(\"replication-code-pcod.R\")'")
 
 setwd(here::here())
